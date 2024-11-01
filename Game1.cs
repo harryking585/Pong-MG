6bc8705229527dbd84e156e1e8814f9f2ba97a66
HK <harryking585@gmail.com>
2024-11-01 12:30:43 -0500

    Updated content, added bumper sprite

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,6 +10,12 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    public static int ScreenWidth;
+    public static int ScreenHeight;
+    public static Random Random;
+    Vector2 test_position;
+
+    Texture2D p1_bumper;
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -16,20 +23,28 @@ public class Game1 : Game
         IsMouseVisible = true;
     }
 
+    // Create memory-only objects
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        ScreenWidth = _graphics.PreferredBackBufferWidth;
+        ScreenHeight = _graphics.PreferredBackBufferHeight;
+        Random = new Random();
 
+        test_position = new Vector2(30,30);
+        
         base.Initialize();
     }
 
+    // Load and create file-related objects
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+        p1_bumper = Content.Load<Texture2D>("bumper");
         // TODO: use this.Content to load your game content here
     }
 
+    // Game logic
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -40,12 +55,16 @@ public class Game1 : Game
         base.Update(gameTime);
     }
 
+    // Drawing logic
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
 
+        _spriteBatch.Draw(p1_bumper, test_position, Color.White);
+        
         base.Draw(gameTime);
     }
 }
